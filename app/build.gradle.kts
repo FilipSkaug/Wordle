5427732b9cfea2ef6103e6a558c0bfca7897ae15
Elias Borge-Ilseth <eliasborgeilseth@gmail.com>
2026-04-14 12:50:15 +0200

    Add Main Menu, and change Main activity accordingly. Update dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(platform("com.google.firebase:firebase-bom:34.10.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation ("com.google.firebase:firebase-firestore")
